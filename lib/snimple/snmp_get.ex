59c306e23cf5e3bd1378e4bd3395e768c412c478
Stuart Osborne <stuart.osborne@transmode.se>
2015-08-21 22:36:24 +0200

    Basic encoding started and tests.

--- lib/snimple/snmp_get.ex
+++ lib/snimple/snmp_get.ex
@@ -2,5 +2,27 @@ defmodule Snimple.SNMPGet do
 	def make_snmp_get() do
 		<<11>>
 	end
+
+	def ber(:int32, value) when is_integer(value) do
+		as_bin = << value::size(32) >>
+		sig = signify(as_bin)
+		<< 2 >> <> << byte_size(sig) >> <> sig
+	end
+
+	def signify(value) when is_binary(value) do
+		_signify(value)
+	end
+	defp _signify(<< 0, 0, 0, x::binary >>) do
+		x
+	end
+	defp _signify(<< 0, 0, x::binary >>) do
+		x
+	end
+	defp _signify(<< 0, x::binary >>) do
+		x
+	end
+	defp _signify(<< x::binary >>) do
+		x
+	end
 	
 end
--- test/snmp_get_test.exs
+++ test/snmp_get_test.exs
@@ -7,9 +7,21 @@ defmodule SNMPGetTest do
 		{:ok, pkt} = Base.decode16("303102010104067075626c6963a02402047f71fce70201000201003016301406102b06010401c40402030204010104817d0500", [case: :lower])
 		pkt
 	end
+
+	test "signify should strip leading 0's in a binary" do
+		assert signify(<<0, 0, 0, 10>>) == <<10>>
+	end
 	
 	test "should be able to construct an snmp get message" do
 		assert make_snmp_get() == example_message
+	end
+
+	test "should be able to encode an integer32 according to BER (Basic Encoding Rules) and get 1 byte" do
+		assert ber(:int32, 8) == << 2, 1, 8 >>
+	end
+
+	test "should be able to encode an integer32 according to BER (Basic Encoding Rules) and get 2 bytes" do
+		assert ber(:int32, 256) == << 2, 2, 1, 0 >>
 	end
 	
 end
