398eb3d53d24230c698d1e455fe495be5cb2084c
Stuart Osborne <stuart.osborne@transmode.se>
2015-08-24 15:55:10 +0200

    More BER work.

--- lib/snimple/snmp_get.ex
+++ lib/snimple/snmp_get.ex
@@ -1,5 +1,5 @@
 defmodule Snimple.SNMPGet do
-	
+
 	def make_snmp_get() do
 		<<11>>
 	end
@@ -13,17 +13,18 @@ defmodule Snimple.SNMPGet do
 	def ber(:octetstring, value) when is_binary(value) do
 		<< 4 >> <> << byte_size(value) >> <> value
 	end
-	
+
 	@doc """
 	BER for an OId is as follows:
-	1) The first two nodes of the OID are encoded onto a single byte. 
-	   The first node is multiplied by the decimal 40 and the result 
+	1) The first two nodes of the OID are encoded onto a single byte.
+	   The first node is multiplied by the decimal 40 and the result
 		 is added to the value of the second node.
 	2) Node values less than or equal to 127 are encoded on one byte.
-	3) Node values greater than or equal to 128 are encoded on 
-	   multiple bytes. Bit 7 of the leftmost byte is set to one, 
-		 when there are more byes to come, zero otherwise. 
+	3) Node values greater than or equal to 128 are encoded on
+	   multiple bytes. Bit 7 of the leftmost byte is set to one,
+		 when there are more byes to come, zero otherwise.
 		 Bits 0 through 6 of each byte contains the encoded value.
+		 http://forcedfx.blogspot.se/2010/07/ber-encoding-snmp-oid.html
 		 """
 	def ber(:oid, oid_string) do
 		oid_nodes = oid_string |> String.strip(?.) |> String.split(".") |> Enum.map(fn nr -> String.to_integer(nr) end)
@@ -44,9 +45,10 @@ defmodule Snimple.SNMPGet do
 	end
 
 	def encode_node(oid_node) do
-		<< 1 >>
+		bytes = << oid_node::size(32) >> | strip_zero_bytes
+		
 	end
-			
+
 	def strip_zero_bytes(value) when is_binary(value) do
 		_strip_zero_bytes(value)
 	end
