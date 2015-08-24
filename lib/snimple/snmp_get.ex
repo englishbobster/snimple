2ff46542b304dc298192ac344e055db57ec9907a
Stuart Osborne <stuart.osborne@transmode.se>
2015-08-24 06:52:48 +0200

    Some work on BER.

--- lib/snimple/snmp_get.ex
+++ lib/snimple/snmp_get.ex
@@ -1,28 +1,65 @@
 defmodule Snimple.SNMPGet do
+	
 	def make_snmp_get() do
 		<<11>>
 	end
 
 	def ber(:int32, value) when is_integer(value) do
 		as_bin = << value::size(32) >>
-		sig = signify(as_bin)
+		sig = strip_zero_bytes(as_bin)
 		<< 2 >> <> << byte_size(sig) >> <> sig
 	end
 
-	def signify(value) when is_binary(value) do
-		_signify(value)
-	end
-	defp _signify(<< 0, 0, 0, x::binary >>) do
-		x
-	end
-	defp _signify(<< 0, 0, x::binary >>) do
-		x
-	end
-	defp _signify(<< 0, x::binary >>) do
-		x
-	end
-	defp _signify(<< x::binary >>) do
-		x
+	def ber(:octetstring, value) when is_binary(value) do
+		<< 4 >> <> << byte_size(value) >> <> value
 	end
 	
+	@doc """
+	BER for an OId is as follows:
+	1) The first two nodes of the OID are encoded onto a single byte. 
+	   The first node is multiplied by the decimal 40 and the result 
+		 is added to the value of the second node.
+	2) Node values less than or equal to 127 are encoded on one byte.
+	3) Node values greater than or equal to 128 are encoded on 
+	   multiple bytes. Bit 7 of the leftmost byte is set to one, 
+		 when there are more byes to come, zero otherwise. 
+		 Bits 0 through 6 of each byte contains the encoded value.
+		 """
+	def ber(:oid, oid_string) do
+		oid_nodes = oid_string |> String.strip(?.) |> String.split(".") |> Enum.map(fn nr -> String.to_integer(nr) end)
+		{[a, b], oid_tail} = oid_nodes |> Enum.split(2)
+		<< 6 >> <> << byte_size <<1>>  >> <> << a*40 + b >>
+	end
+	def ber(:null) do
+		<< 5, 0 >>
+	end
+
+	def encode_tail(oid_node) do
+		cond do
+			oid_node >= 128 ->
+			  encode_node(oid_node)
+			true ->
+				<< oid_node >>
+		end
+	end
+
+	def encode_node(oid_node) do
+		<< 1 >>
+	end
+			
+	def strip_zero_bytes(value) when is_binary(value) do
+		_strip_zero_bytes(value)
+	end
+	defp _strip_zero_bytes(<< 0, 0, 0, x::binary >>) do
+		x
+	end
+	defp _strip_zero_bytes(<< 0, 0, x::binary >>) do
+		x
+	end
+	defp _strip_zero_bytes(<< 0, x::binary >>) do
+		x
+	end
+	defp _strip_zero_bytes(<< x::binary >>) do
+		x
+	end
 end
--- test/snmp_get_test.exs
+++ test/snmp_get_test.exs
@@ -8,20 +8,44 @@ defmodule SNMPGetTest do
 		pkt
 	end
 
-	test "signify should strip leading 0's in a binary" do
-		assert signify(<<0, 0, 0, 10>>) == <<10>>
+	def test_string do
+		"a test octet string"
 	end
-	
+
 	test "should be able to construct an snmp get message" do
 		assert make_snmp_get() == example_message
 	end
 
-	test "should be able to encode an integer32 according to BER (Basic Encoding Rules) and get 1 byte" do
+	#tests of encoding utilities
+	test "should be able to strip leading 0's in a binary" do
+		assert strip_zero_bytes(<<0, 0, 0, 10>>) == <<10>>
+	end
+
+	test "should encode oid node greater than or equal to 128" do
+		assert encode_node(8708) == << 0x82, 0x37 >>
+	end
+
+	#tests of Basic Encoding Rules (BER)
+	test "should be able to encode an integer32 according to BER" do
 		assert ber(:int32, 8) == << 2, 1, 8 >>
 	end
 
-	test "should be able to encode an integer32 according to BER (Basic Encoding Rules) and get 2 bytes" do
+	test "should be able to encode an integer32 according to BER" do
 		assert ber(:int32, 256) == << 2, 2, 1, 0 >>
+	end
+
+	test "should be able to encode null value according to BER" do
+		assert ber(:null) == << 5, 0 >>
+	end
+
+	test "should be able to encode an octetstring according to BER" do
+		test_string_size = byte_size(test_string)
+		assert ber(:octetstring, test_string) == << 4 >> <> <<test_string_size>> <> test_string
+	end
+
+	test "should be able to encode an OID according to BER" do
+		oid = ".1.3.6.1.4.1.8708.2.1.2.2.1.1.3.237"
+		assert ber(:oid, oid) == <<6, 16, 43, 6, 1, 4, 1, 196, 4, 2, 4, 2, 2, 1, 1, 72, 141, 3>>
 	end
 	
 end
