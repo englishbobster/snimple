ef4ee8dda51ecd2a0434f208bb73b09e3c06628c
Stuart Osborne <stuart.osborne@transmode.se>
2015-08-28 07:36:12 +0200

    More implementation.

--- lib/snimple/ber.ex
+++ lib/snimple/ber.ex
@@ -1,7 +1,9 @@
 defmodule Snimple.BER do
+
+	require Bitwise
 	
 	def ber(:int32, value) when is_integer(value) do
-		value_as_bin = :erlang.encode_unsigned(value)
+		value_as_bin = :binary.encode_unsigned(value)
 		<< 2 >> <> << byte_size(value_as_bin) >> <> value_as_bin
 	end
 
@@ -20,15 +22,15 @@ defmodule Snimple.BER do
 	end
 	
 	def encode_oid_node(node) do
-		node_as_bin = :erlang.encode_unsigned(node)
-		current_size = byte_size(node_as_bin)
-		current = Bitmask.&&&(node_as_bin, 0x7F)
-		_encode(Bitmask.>>>(node_as_bin, 7), current, current_size - 1)
+		size = :binary.encode_unsigned(node) |> bit_size()
+		value = << Bitwise.&&&(node, 0x7F) >>
+		_encode(Bitwise.>>>(node, 7), value, size - 7)
 	end
-	defp _encode(bin, current, remaining_bytes) do
-
-	end
-	defp _encode(_, current,  0) do
+	defp _encode(_, current, value) when value <= 0 do
 		current
+	end
+	defp _encode(value, current, remaining_bits) do
+		val = Bitwise.&&&(value, 0x7F) |> Bitwise.|||(0x80)
+		_encode(Bitwise.>>>(val, 7), << val >> <> current, remaining_bits - 7) 
 	end
 end
--- test/ber_test.exs
+++ test/ber_test.exs
@@ -26,11 +26,12 @@ defmodule BERTest do
 
 	test "should be able to encode an OID according to BER" do
 		oid = ".1.3.6.1.4.1.8708.2.1.2.2.1.1.3.237"
-		assert ber(:oid, oid) == <<6, 16, 43, 6, 1, 4, 1, 196, 4, 2, 4, 2, 2, 1, 1, 72, 141, 3>>
+		assert ber(:oid, oid) == << 6, 16, 43, 6, 1, 4, 1, 196, 4, 2, 4, 2, 2, 1, 1, 72, 141, 3 >>
 	end
 
 	test "should encode oid node greater than or equal to 128" do
-		assert encode_oid_node(8708) == << 0x82, 0x37 >>
+		assert encode_oid_node(8708) == << 0xC4, 0x04 >>
+		assert encode_oid_node(19865) == << 0x81, 0x9B, 0x19 >>
 	end
 	
 end
