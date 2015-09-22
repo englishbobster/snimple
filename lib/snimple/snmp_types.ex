268823d360cbb7b00dd0c6b7afcf9147d825238f
Stuart Osborne <stuart.osborne@transmode.se>
2015-09-22 18:54:21 +0200

    A ghost of an implementation of snmp_types.

--- lib/snimple/snmp_types.ex
+++ lib/snimple/snmp_types.ex
@@ -1,3 +1,24 @@
 defmodule Snimple.SNMP.Types do
 
+	def encode(value, :int32) do
+	end
+
+	def encode(ipaddress, :ipaddr) do
+	end
+
+	def encode(value, :counter32) do
+	end
+
+	def encode(value, :gauge32) do
+	end
+
+	def encode(ticks, :timeticks) do
+	end
+	
+	def encode(value, :counter64) do
+	end
+
+	def decode(<< data::binary >>) do
+	end
+		
 end
